8a11a4272dc58c4abaea9b270d0d041cd4b49486
Bart Groothuysen <bart@groothuysen.com>
2023-04-14 11:40:39 +0200

    wijzinging 14-4-2023 (1)

--- site/index.php
+++ site/index.php
@@ -16,41 +16,37 @@ require 'database.php';
 <div class="container">
     <main>
         <form action="" method="get">
-            <div class="input-groep">
-                <div class="test">
-                <ul>
-                    <li>
-                        <label for="txtFirst">Your name:</label><br>
-                        <input type="text" id="txtFirst" name="txtFirst" placeholder="First" autofocus>
-                    </li>
-                    <li>
-                        <input type="text" id="txtLast" name="txtLast" placeholder="Last">
-                    </li>
-                </ul>
-                <ul>
-                    <li>
-                        <label for="email">E-mail:</label><br>
-                        <input type="email" id="txtemail" name="txtemail">
-                    </li>                  
-                </ul>
-                <ul>
-                    <li>
-                        <label for="Phone">Phone:</label><br>
-                        <input type="tel" id="txtPhone" name="txtPhone" placeholder="### ### ###" autofocus>
-                    </li>                 
-                </ul>                          
-                    <ul>
-                        <li>
-                            <label for="message">Your message: </label><br>
-                            <textarea name="message" id="txtmessage" cols="65" rows="10" autofocus></textarea>
-                        </li>                       
-                    </ul>
-                    <li>
-                        <button type="submit">Verstuur</button>
-                    </li>
-                </ul>
-                </div>
-            </div>
+            <ul>
+                <li>
+                    <label for="txtFirst">Your name:</label><br>
+                    <input type="text" id="txtFirst" name="txtFirst" placeholder="First" autofocus>
+                </li>
+                <li>
+                    <input type="text" id="txtLast" name="txtLast" placeholder="Last">
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <label for="email">E-mail:</label><br>
+                    <input type="email" id="txtemail" name="txtemail">
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <label for="Phone">Phone:</label><br>
+                    <input type="tel" id="txtPhone" name="txtPhone" placeholder="### ### ###" autofocus>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <label for="message">Your message: </label><br>
+                    <textarea name="message" id="txtmessage" cols="65" rows="10" autofocus></textarea>
+                </li>
+            </ul>
+            <li>
+                <button type="submit">Verstuur</button>
+            </li>
+            </ul>
         </form>
     </main>
 </div>
