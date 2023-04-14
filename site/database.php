5d55cccc8cb4c816108e19ee115103186cb2e5d2
Bart Groothuysen <bart@groothuysen.com>
2023-04-14 11:05:50 +0200

    database.php aangemaakt

--- site/database.php
+++ site/database.php
@@ -8,12 +8,4 @@ $dbname = "feoSpeeltuin";
 // Maak een  database connectie
 $conn = mysqli_connect($host, $dbuser, $dbpass, $dbname);
 
-// Controleer de verbinding
-if(mysqli_connect_error())
-{
- echo "Connection establishing failed!";
-}
-else
-{
- echo "Connection established successfully.";
-}
+
