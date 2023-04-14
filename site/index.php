e32cfbe0e37ececc26107213739d97b1f920ad8d
Bart Groothuysen <bart@groothuysen.com>
2023-04-14 11:18:00 +0200

    css styling toegevoegd aan index.php 1

--- site/index.php
+++ site/index.php
@@ -1,5 +1,5 @@
-<?php 
-  require 'database.php';
+<?php
+require 'database.php';
 ?>
 
 
@@ -11,16 +11,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
-    <div class="container">
+<div class="container">
     <main>
         <form action="" method="get">
-
+            
         </form>
     </main>
-    </div>
+</div>
+
 <body>
-    
+
 </body>
 
 </html>
