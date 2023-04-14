99aafe71b75e98cfbfbf135772564828a1f0e676
Bart Groothuysen <bart@groothuysen.com>
2023-04-14 11:02:34 +0200

    database configuratie code toegevoegd

---
+++ site/database.php
@@ -0,0 +1,9 @@
+// Database configuratie
+<?
+$host  = "mariadb";
+$dbuser = "root";
+$dbpass = "password";
+$dbname = "feoSpeeltuin";
+
+// Maak een  database connectie
+$conn = mysqli_connect($host, $dbuser, $dbpass, $dbname);
