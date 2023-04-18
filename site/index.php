8b1ef273ddee4e0db9610c31c84c9ef5bb0f15c0
Bart Groothuysen <bart@groothuysen.com>
2023-04-18 09:36:17 +0200

    structuur netter gemaakt

--- site/index.php
+++ site/index.php
@@ -16,39 +16,52 @@ require 'database.php';
 <div class="container">
     <main>
         <form action="" method="get">
-            <ul>
-                <li>
-                    <label for="txtFirst">Your name:</label>
-                    <input type="text" id="txtFirst" name="txtFirst" placeholder="First" autofocus>
-                </li>
-                <li>
-                    <input type="text" id="txtLast" name="txtLast" placeholder="Last">
-                </li>
-            </ul>
-            <ul>
-                <li>
-                    <label for="email">E-mail:</label>
-                    <p class="extra-info">We're going to reply to your message by email</p>
-                    <input type="email" id="txtemail" name="txtemail">
-                </li>
-            </ul>
-            <ul>
-                <li>
-                    <label for="Phone">Phone:</label>
-                    <p class="extra-info">Only if you want to call us</p>
-                    <input type="tel" id="txtPhone" name="txtPhone" placeholder="### ### ###" autofocus>
-                </li>
-            </ul>
+            <div class="groep_types">
+                <ul>
+                    <li>
+                        <label for="txtFirst">Your name:</label>
+                        <input type="text" id="txtFirst" name="txtFirst" placeholder="First" autofocus>
+                    </li>
+
+
+                    <li>
+                        <input type="text" id="txtLast" name="txtLast" placeholder="Last">
+                    </li>
+                </ul>
+            </div>
+            <div class="groep_types">
+                <ul>
+                    <li>
+                        <label for="email">E-mail:</label>
+                        <p class="extra-info">We're going to reply to your message by email</p>
+                        <input type="email" id="txtemail" name="txtemail">
+                    </li>
+                </ul>
+            </div>
+            <div class="groep_types">
+                <ul>
+                    <li>
+                        <label for="Phone">Phone:</label>
+                        <p class="extra-info">Only if you want to call us</p>
+                        <input type="tel" id="txtPhone" name="txtPhone" placeholder="### ### ###" autofocus>
+                    </li>
+                </ul>
+            </div>
+            <div class="groep_types">
             <ul>
                 <li>
                     <label for="message">Your message: </label>
-                    <textarea name="message" id="txtmessage" cols="70" rows="10" autofocus></textarea>
+                    <textarea name="message" id="txtmessage" cols="67" rows="10" autofocus></textarea>
                 </li>
             </ul>
+            </div>
+            <div class="groep_button">
+            <ul>
             <li>
                 <button type="submit">Send</button>
             </li>
             </ul>
+            </div>
         </form>
     </main>
 </div>
