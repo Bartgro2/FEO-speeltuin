dde9abe7f541c1e46218944d82b7596612fc16c1
Bart Groothuysen <bart@groothuysen.com>
2023-04-21 11:41:54 +0200

    laatste wijzingen

---
+++ site/index2.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style2.css">
+</head>
+<div class="container">
+    <main>
+        <form action="" method="get">
+            <div class="top-form">
+                <h1>SATISFACTION SURVEY 2023</h1>
+            </div>
+            <div class="groep_types">
+
+                <ul>
+                    <li>
+                        <label for="radio">What is your age group</label>
+                        <div class="blok">
+                            <div class="blokje">
+                                <input type="radio" name="age" id="Under_18">
+                                <label for="age">Under 18</label>
+                            </div>
+
+
+                            <input type="radio" name="age" id="18-24">
+
+
+                            <input type="radio" name="age" id="25-34">
+
+
+                            <input type="radio" name="age" id="35-44">
+
+                        </div>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="groep_button">
+                <ul>
+                    <li>
+                        <button type="submit">SEND</button>
+                    </li>
+                </ul>
+            </div>
+        </form>
+    </main>
+</div>
+
+<body>
+
+</body>
+
+</html>
